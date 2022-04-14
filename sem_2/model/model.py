02f561cabfab989316866b6b1f02a4f96fa42aaf
fukinii <fookinilia2013@gmail.com>
2022-04-14 16:02:54 +0300

    Добавление методов в класс модели

--- sem_2/model/model.py
+++ sem_2/model/model.py
@@ -4,10 +4,28 @@ from .consts import *
 
 class PhotoionizationModel:
 
-    def __init__(self):
+    def __init__(self, n):
         self.equationNumber = 3
+        self.n = n
 
         pass
+
+    def calc_rhs(self, alpha, T_e, T, r, t):
+        res = np.zeros(self.equationNumber)
+        j_ei = self.calc_j_ei(T_e=T_e)
+        j_nu_ei = self.calc_j_nu_ei(T_e=T_e)
+        K = self.calc_K(T_e=T_e)
+        j_v = self.calc_j_v(r, t)
+        nu_e_mean = self.calc_v_e_mean(T_e=T_e)
+        nu_e = self.calc_nu_e(v_e_mean=nu_e_mean, alpha=alpha, T_e=T_e)
+        res[0] = ((1 - alpha) + j_v - alpha * alpha * self.n * j_nu_ei) + \
+                 self.n * alpha * j_ei * ((1 - alpha) * K - self.n * alpha(alpha))
+        res[1] = (T - T_e) * nu_e - (
+                2. * I / 3 + T_e) * self.n * j_ei * ((1 - alpha) * K - self.n * alpha * alpha) + T_e * (
+                         1 - 2. * self.calc_F(T_e=T_e) / 3) * alpha * self.n * j_nu_ei + (2. * T_v / 3. - T_e) * (
+                         1 - alpha) / alpha * j_v
+        res[2] = - (T - T_e) * alpha * nu_e
+        return res
 
     @staticmethod
     def calc_j_nu_0():
@@ -40,12 +58,21 @@ class PhotoionizationModel:
         res = pow(10, res_)
         return res
 
-    @staticmethod
-    def calc_nu_e(n, v_e_mean, alpha, T_e):
-        res = n * 2 * m_e / m * (
+    def calc_nu_e(self, v_e_mean, alpha, T_e):
+        res = self.n * 2 * m_e / m * (
                 4. / 3. * sigma_e_0 * v_e_mean * (1 - alpha) +
                 4 * np.sqrt(2 * np.pi) / 3 * alpha * Lambda * pow(e, 4) / (np.sqrt(m_e) * pow(1.6e-12 * T_e, 3. / 2)))
         return res
 
-    def calc_j_v(self):
+    @staticmethod
+    def calc_v_e_mean(T_e):
+        res = np.sqrt(8 * (1.6e-12 * T_e) / (np.pi * m_e))
+        return res
+
+    @staticmethod
+    def calc_tau_0():
         pass
+
+    @staticmethod
+    def calc_j_v(r, t):
+        return 1.
